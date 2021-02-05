4faa418a69e0a65dce42a3ab87d0f8aa05c75aca
Tim Dévé <TimDeve@users.noreply.github.com>
2021-02-05 11:31:09 +0000

    Makes repository function generic over DB executor

--- server/src/repository.rs
+++ server/src/repository.rs
@@ -1,24 +1,30 @@
 use crate::domain::{Food, NewFood};
-use sqlx::PgPool;
+use sqlx::{Executor,Postgres};
 
 #[derive(sqlx::FromRow)]
 struct Id(i32);
 
-pub async fn get_foods(pool: &PgPool) -> anyhow::Result<Vec<Food>> {
+pub async fn get_foods<'a, E>(exec: E) -> anyhow::Result<Vec<Food>>
+where
+    E: 'a + Executor<'a, Database = Postgres>,
+{
     let foods = sqlx::query_as("SELECT id, name, best_before_date FROM foods;")
-        .fetch_all(pool)
+        .fetch_all(exec)
         .await?;
 
     Ok(foods)
 }
 
-pub async fn create_food(pool: &PgPool, food: NewFood) -> anyhow::Result<Food> {
+pub async fn create_food<'a, E>(exec: E, food: NewFood) -> anyhow::Result<Food>
+where
+    E: 'a + Executor<'a, Database = Postgres>,
+{
     let Id(id) = sqlx::query_as(
         "INSERT INTO foods ( name, best_before_date ) VALUES ( $1, $2 ) RETURNING id;",
     )
     .bind(&food.name)
     .bind(&food.best_before_date)
-    .fetch_one(pool)
+    .fetch_one(exec)
     .await?;
 
     Ok(Food {
@@ -28,10 +34,13 @@ pub async fn create_food(pool: &PgPool, food: NewFood) -> anyhow::Result<Food> {
     })
 }
 
-pub async fn delete_food(pool: &PgPool, id: i32) -> anyhow::Result<()> {
+pub async fn delete_food<'a, E>(exec: E, id: i32) -> anyhow::Result<()>
+where
+    E: 'a + Executor<'a, Database = Postgres>,
+{
     sqlx::query("DELETE FROM foods WHERE id = $1;")
         .bind(id)
-        .execute(pool)
+        .execute(exec)
         .await?;
 
     Ok(())
