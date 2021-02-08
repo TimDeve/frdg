d48dc7fd0a8a1793a92fcff461114a0efd34928f
Tim Dévé <TimDeve@users.noreply.github.com>
2021-02-08 17:27:48 +0000

    Creates new food section in UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,15 @@ import {
   CardContent,
   CardActions,
   Button,
+  Fab,
+  makeStyles,
+  TextField,
 } from "@material-ui/core"
+import AddIcon from "@material-ui/icons/Add"
+import CloseIcon from "@material-ui/icons/Close"
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers"
+import DayjsUtils from "@date-io/dayjs"
+import dayjs, {Dayjs} from 'dayjs'
 
 import { deleteFood, getFoods } from "./gateway"
 import { Food } from "./domain"
@@ -36,16 +44,29 @@ const queryClient = new QueryClient()
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Page />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <MuiPickersUtilsProvider utils={DayjsUtils}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Page />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </MuiPickersUtilsProvider>
   )
 }
 
+const useStyles = makeStyles(theme => ({
+  fab: {
+    position: "fixed",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}))
+
 function Page() {
+  const styles = useStyles()
+  const [newFoodOpen, setNewFoodOpen] = useState(true)
+
   return (
     <>
       <AppBar position="static">
@@ -56,9 +77,55 @@ function Page() {
         </Toolbar>
       </AppBar>
       <Container maxWidth="sm">
+        {newFoodOpen && <NewFoodForm />}
         <FoodList />
       </Container>
+      <Fab
+        className={styles.fab}
+        color="primary"
+        onClick={() => setNewFoodOpen(!newFoodOpen)}
+      >
+        {newFoodOpen ? <CloseIcon /> : <AddIcon />}
+      </Fab>
     </>
+  )
+}
+
+function NewFoodForm() {
+  const [name, setName] = useState("")
+  const [date, setDate] = useState<Dayjs | null>(dayjs())
+
+  return (
+    <Card style={{ marginTop: "14px", marginBottom: "14px" }}>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          console.log(name, date)
+        }}
+      >
+        <CardContent>
+          <TextField
+            value={name}
+            name="name"
+            placeholder="Food Name"
+            onChange={e => setName(e.target.value)}
+          />
+          <br/>
+          <DatePicker
+            name="date"
+            value={date}
+            onChange={newDate => {
+              setDate(newDate)
+            }}
+          />
+        </CardContent>
+        <CardActions>
+          <Button type="submit" size="small">
+            Add
+          </Button>
+        </CardActions>
+      </form>
+    </Card>
   )
 }
 
@@ -75,7 +142,7 @@ function FoodList() {
 
   return (
     <>
-      {data.map((food) => (
+      {data.map(food => (
         <FoodItem key={food.id} {...food} />
       ))}
     </>
--- client/src/gateway.ts
+++ client/src/gateway.ts
@@ -1,3 +1,4 @@
+import { Dayjs } from "dayjs"
 import { Food } from "./domain"
 
 interface GetFoodsResponse {
@@ -10,6 +11,22 @@ export async function getFoods(): Promise<Food[]> {
     if (!res.ok) throw new Error("Failed to fetch foods")
     const json: GetFoodsResponse = await res.json()
     return json.foods
+  } catch (e) {
+    console.error(e)
+    throw e
+  }
+}
+
+export async function createFood(name: string, date: Dayjs): Promise<void> {
+  try {
+    const res = await fetch('/api/v0/foods', {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        bestBeforeDate: date.format("YYYY-MM-DD")
+      })
+    })
+    if (!res.ok) throw new Error("Failed to create food")
   } catch (e) {
     console.error(e)
     throw e
